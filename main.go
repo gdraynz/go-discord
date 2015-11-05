74d323ead8ef0d4b673e754c3c2798caaaac96c1
Guillaume Desvé <guillaume.desve@surycat.com>
2015-11-05 11:27:41 +0100

    Update main.go with a generic bot (commands: uptime, help)

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/gdraynz/go-discord/discord"
 )
@@ -13,33 +16,77 @@ import (
 var (
 	flagConf = flag.String("conf", "conf.json", "Configuration file")
 
-	client discord.Client
+	client    discord.Client
+	startTime time.Time
+	commands  map[string]Command
 )
 
+type Command struct {
+	Word    string
+	Help    string
+	Handler func(ChannelID string)
+}
+
+func onReady(ready discord.Ready) {
+	startTime = time.Now()
+}
+
 func messageReceived(message discord.Message) {
-	_, ok := client.PrivateChannels[message.ChannelID]
+	if !strings.HasPrefix(message.Content, "!go") {
+		return
+	}
+
+	args := strings.Split(message.Content, " ")
+	if len(args) < 2 {
+		return
+	}
+
+	command, ok := commands[args[1]]
 	if ok {
-		switch message.Content {
-		case "info":
-			client.SendMessage(message.ChannelID, "Your name: "+message.Author.Name)
-			client.SendMessage(message.ChannelID, "Your email: "+message.Author.Email)
-		}
+		command.Handler(message.ChannelID)
+	} else {
+		log.Printf("No command '%s'", args[1])
 	}
 }
 
-func typingMessage(typing discord.Typing) {
-	_, ok := client.PrivateChannels[typing.ChannelID]
-	if ok {
-		client.SendMessage(typing.ChannelID, "DON'T TALK TO ME")
+func helpCommand(channelID string) {
+	toSend := "Available commands:\n"
+	for _, command := range commands {
+		toSend += fmt.Sprintf("`%s` %s\n", command.Word, command.Help)
 	}
+	client.SendMessage(channelID, toSend)
+}
+
+func uptimeCommand(channelID string) {
+	uptime := time.Now().Sub(startTime)
+	toSend := fmt.Sprintf(
+		"`Uptime` %0.2d:%02d:%02d",
+		int(uptime.Hours()),
+		int(uptime.Minutes()),
+		int(uptime.Seconds()),
+	)
+	client.SendMessage(channelID, toSend)
 }
 
 func main() {
 	flag.Parse()
 
 	client = discord.Client{
+		OnReady:         onReady,
 		OnMessageCreate: messageReceived,
-		OnTypingStart:   typingMessage,
+	}
+
+	commands = map[string]Command{
+		"uptime": Command{
+			Word:    "uptime",
+			Help:    "Shows the bot's uptime",
+			Handler: uptimeCommand,
+		},
+		"help": Command{
+			Word:    "help",
+			Help:    "Prints the help message",
+			Handler: helpCommand,
+		},
 	}
 
 	sigc := make(chan os.Signal, 1)
